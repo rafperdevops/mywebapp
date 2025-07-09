f9af86817ea8cf0810407551e952da631ea3dd20
rafperdevops <rafperdevops@gmail.com>
2025-07-09 18:45:34 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return """
+    <h2>Welcome</h2>
+    <img src="https://png.pngtree.com/thumb_back/fh260/background/20230521/pngtree-sunflower-full-screen-backdrop-widescreen-photos-image_2684387.jpg" />
+"""
+
+@app.route("/saludos")
+def hello():
+    return "<h1>Hola mundo!!!</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
